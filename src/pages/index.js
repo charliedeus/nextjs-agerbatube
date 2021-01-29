9197b09fd31e49b3d24dd1cc6e36a4698db6500b
Charles Loureiro de Deus <charliedeus@gmail.com>
2021-01-28 21:47:04 -0300

    Material UI Configuration

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+
+function Home() {
+  return (
+    <div>
+      <div>Clone youtube com NextJS + Material-UI (without TypeScript)</div>
+      <Button variant="outlined" color="secondary">
+        Primary
+      </Button>
+    </div>
+  )
+}
+
+export default Home
