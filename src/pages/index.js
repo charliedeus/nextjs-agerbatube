92c2b8376b4f15b0f9613cf665d23485363a28b1
Charles Loureiro de Deus <charliedeus@gmail.com>
2021-02-02 19:14:59 -0300

    Adding static data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,120 @@
 import React from 'react'
-import { Button } from '@material-ui/core'
+import { Box, Grid } from '@material-ui/core'
 
-import Layout from '../components/Layout'
+import Layout from 'src/components/Layout'
+import VideoCard from 'src/components/VideoCard'
 
-function Home() {
+function Home({ data }) {
   return (
     <Layout title="AGERBATube">
-      <div>Clone youtube com NextJS + Material-UI (without TypeScript)</div>
-      <Button variant="outlined" color="secondary">
-        Primary
-      </Button>
+      <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item._id} item xl={3} lg={3} md={4} sm={6} xs={12}>
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const data = [
+    {
+      id: 1,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCE DEVERIA CONHECER [PARTE #01]',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCE DEVERIA CONHECER [PARTE #02]',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next02.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 3,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCE DEVERIA CONHECER [PARTE #03]',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 4,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCE DEVERIA CONHECER [PARTE #04]',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 5,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCE DEVERIA CONHECER [PARTE #01]',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 6,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCE DEVERIA CONHECER [PARTE #02]',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next02.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 7,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCE DEVERIA CONHECER [PARTE #03]',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 8,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCE DEVERIA CONHECER [PARTE #04]',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+  ]
+
+  return {
+    props: { data: JSON.parse(JSON.stringify(data)) }, // will be passed to the page component as props
+  }
 }
 
 export default Home
